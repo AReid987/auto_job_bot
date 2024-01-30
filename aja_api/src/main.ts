f6085dc77fba7dada6e9e05900467d6a5bd66fae
Antonio Reid <read.musik@gmail.com>
2024-01-30 17:51:21 -0600

    added swc for Nest JS v10

--- aja_api/src/main.ts
+++ aja_api/src/main.ts
@@ -4,11 +4,12 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-
+// *TODO - Add fastify-swagger
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
+    { snapshot: true },
   );
   await app.listen(3000);
 }
