1054701525b713f9e7764c681c4e499207d0451b
Antonio Reid <read.musik@gmail.com>
2024-01-30 22:18:56 -0600

    added swagger

--- aja_api/src/app.module.ts
+++ aja_api/src/app.module.ts
@@ -1,15 +1,23 @@
+/* The AppModule class is a module in a NestJS application that imports the DevtoolsModule and
+registers the AppController and AppService. */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DevtoolsModule } from '@nestjs/devtools-integration';
 
+// *ANCHOR - The @Module() decorator is used to define a module and its configuration.
 @Module({
+  /* The imports array is used to import other modules that this module depends on. */
   imports: [
     DevtoolsModule.register({
+      // Registering the DevtoolsModule
       http: process.env.NODE_ENV !== 'production',
     }),
   ],
+  /* The controllers array is used to specify the controllers that belong to this module. */
   controllers: [AppController],
+  /* The providers array is used to specify the services that belong to this module. */
   providers: [AppService],
 })
+/* The AppModule class is exported so that it can be used by other modules. */
 export class AppModule {}
--- aja_api/src/main.ts
+++ aja_api/src/main.ts
@@ -4,13 +4,36 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-// *TODO - Add fastify-swagger
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+// * SECTION -  This function initializes the Nest.js application.
+// * @param {NestFactory} NestFactory - The Nest factory class.
+// * ANCHOR -  @param {AppModule} AppModule - The root application module.
+//  * @param {FastifyAdapter} FastifyAdapter - The Fastify adapter class.
+//  * @param {object} options - The application configuration options.
+//  * @param {boolean} [options.snapshot=true] - Whether to enable the Nest snapshot feature.
+//  * @returns {Promise<NestFastifyApplication>} The initialized Nest application.
+
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
     { snapshot: true },
   );
+  const options = new DocumentBuilder()
+    .setTitle('Auto Job App API')
+    .setDescription(
+      'Your very own crew of Autonomous AI agents to help you find a job.',
+    )
+    .setVersion('1.0')
+    .addServer('http://localhost:3000', 'Local development server')
+    .addServer('https://production-url-coming.soon', 'Production server')
+    .addTag('Auto Job App API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
